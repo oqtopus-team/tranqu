3b1ddfd01dd163f9b2307ec64660702d0455883c
Yasuhito Takamiya <yasuhito@hey.com>
2024-12-20 16:50:37 +0900

    add allow_override parameter to DeviceConverterManager.register_converter

--- src/tranqu/device_converter/__init__.py
+++ src/tranqu/device_converter/__init__.py
@@ -3,6 +3,7 @@ from .device_converter_manager import (
     DeviceConverterAlreadyRegisteredError,
     DeviceConverterError,
     DeviceConverterManager,
+    DeviceConverterNotFoundError,
 )
 from .oqtopus_to_qiskit_device_converter import OqtoqusToQiskitDeviceConverter
 from .pass_through_device_converter import PassThroughDeviceConverter
@@ -13,6 +14,7 @@ __all__ = [
     "DeviceConverterAlreadyRegisteredError",
     "DeviceConverterError",
     "DeviceConverterManager",
+    "DeviceConverterNotFoundError",
     "OqtoqusToQiskitDeviceConverter",
     "PassThroughDeviceConverter",
     "QiskitDevice",
