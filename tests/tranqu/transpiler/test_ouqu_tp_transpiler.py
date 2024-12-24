de1a600eaa79c3536559f37b8610a824fa2538ab
Yasuhito Takamiya <yasuhito@hey.com>
2024-12-24 11:59:40 +0900

    add more tests

--- tests/tranqu/transpiler/test_ouqu_tp_transpiler.py
+++ tests/tranqu/transpiler/test_ouqu_tp_transpiler.py
@@ -1,11 +1,16 @@
+import pytest
+from pytket import Circuit as TketCircuit
 from qiskit import QuantumCircuit  # type: ignore[import-untyped]
 
 from tranqu import Tranqu
 
 
 class TestOuquTpTranspiler:
-    def test_transpile_simple_qasm3_program(self):
-        tranqu = Tranqu()
+    @pytest.fixture
+    def tranqu(self) -> Tranqu:
+        return Tranqu()
+
+    def test_transpile_simple_qasm3_program(self, tranqu: Tranqu):
         program = """OPENQASM 3.0;
 include "stdgates.inc";
 qubit[2] q;
@@ -19,8 +24,7 @@ cx q[0],q[1];
 
         assert isinstance(result.transpiled_program, str)
 
-    def test_transpile_qiskit_program(self):
-        tranqu = Tranqu()
+    def test_transpile_qiskit_program(self, tranqu: Tranqu):
         circuit = QuantumCircuit(2)
         circuit.h(0)
         circuit.cx(0, 1)
@@ -30,3 +34,14 @@ cx q[0],q[1];
         )
 
         assert isinstance(result.transpiled_program, QuantumCircuit)
+
+    def test_transpile_tket_program(self, tranqu: Tranqu):
+        circuit = TketCircuit(2)
+        circuit.H(0)
+        circuit.CX(0, 1)
+
+        result = tranqu.transpile(
+            program=circuit, program_lib="tket", transpiler_lib="ouqu-tp"
+        )
+
+        assert isinstance(result.transpiled_program, TketCircuit)
